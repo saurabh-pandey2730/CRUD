0255f847c9cf56cba2f9e652b1dcd3f419d058e3
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-01-31 18:02:07 +0530

    createuser page UI completed

--- client/src/components/CreateUser.jsx
+++ client/src/components/CreateUser.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const CreateUser = () => {
   return (
-    <div>
-      CreateUser
+    <div className='d-flex bg-primary justify-content-center align-items-center vh-100'>
+      <div className=" w-50 bg-white rounded p-3">
+        <form>
+             <h2>Add User</h2>
+            <div className="mb-2">
+                <label  >Name</label>
+                <input type="text" className="form-control" placeholder='Enter Name'/>
+            </div>
+            <div className="mb-2">
+                <label  >Email</label>
+                <input type="email" className="form-control" placeholder='Enter Email'/>
+            </div>
+            <div className="mb-2">
+                <label  >Age</label>
+                <input type="text" className="form-control" placeholder='Enter Name'/>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
